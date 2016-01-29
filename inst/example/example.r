c67082ccd65dfbeef0572cf08053066fa57e1e62
Thibaut Lamadon <thibaut.lamadon@gmail.com>
2016-01-29 00:51:48 -0600

    example is working for 2 periods

--- inst/example/example.r
+++ inst/example/example.r
@@ -31,12 +31,14 @@ sdata[,j1:=clus[sdata,clus]]
 # order by mean wage
 JJ = sdata[,mean(y1),j1][order(j1)][,rank(V1)]
 sdata[,j1 := JJ[j1]]
+clus = unique(sdata[,list(fid=f1,clus=j1)])
 
 # check that the firms are well classified, you should see a strong diagonal
 rr = sdata[,list(j1t=j1t[1],j1=j1),f1][,.N,list(j1,j1t)]
 ggplot(rr,aes(x=j1,y=j1t,size=N)) + geom_point()
 
 # attach clsuter to movers
+setkey(clus,fid)
 jdata[,j1t:=j1] # save true type
 setkey(jdata,f1)
 jdata[,j1:=clus[jdata,clus]]
@@ -44,20 +46,7 @@ jdata[,j2t:=j2] # save true type
 setkey(jdata,f2)
 jdata[,j2:=clus[jdata,clus]]
 
-
-
 # ==== ESTIMATE THE MODEL ========
 
 res = model.mini2.estimate(jdata,sdata,model0 = model)
 
-
-# ==== simulating and estimating the 4 period model ========
-
-# not working just yet!
-
-model  = model.mini4.new(10,r1=0.4,r4=0.8)
-NNm    = array(50000/(model$nf^2),c(model$nf,model$nf))
-NNs    = array(100000/model$nf,model$nf)
-jdata  = model.mini4.simulate.movers(model,NNm)
-sdata  = model.mini4.simulate.stayers(model,NNs)
-model1 = model.mini4.estimate(jdata,sdata,model$r1,model$r4,model0 = model);
